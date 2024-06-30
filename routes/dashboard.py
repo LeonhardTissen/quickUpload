2fc53a7dc4a40d60b83c95a0b5ad001835085eee
Leonhard Tissen <contact@warze.org>
2024-06-30 10:44:18 +0200

    Features: Filename cleanup & Sorted files in dashboard

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,7 +1,6 @@
 from __main__ import app
 from flask import request, render_template, redirect
 import os
-from utils.db import get_users
 from utils.user import get_user
 
 @app.route('/dashboard')
@@ -15,4 +14,5 @@ def dashboard():
 	os.makedirs(f'uploads/{username}', exist_ok=True)
 	filelist = os.listdir(f'uploads/{username}')
 	files = [{'filename': file, 'url': f'/{username}/{file}'} for file in filelist]
+	files = sorted(files, key=lambda x: x['filename'])
 	return render_template('dashboard.html.j2', username=username, id=id, files=files, admin=admin)
--- routes/upload.py
+++ routes/upload.py
@@ -1,7 +1,29 @@
 from __main__ import app
 from flask import request, redirect
-from utils.user import get_user
 import os
+from utils.user import get_user
+
+def is_valid_character(c):
+	return c.isalnum() or c == '.' or c == '_'
+
+def clean_filename(filename):
+	only_alphanumeric = ''.join(c for c in filename if is_valid_character(c)).strip()
+
+	file_ext = only_alphanumeric.split('.')[-1]
+	file_name = '.'.join(only_alphanumeric.split('.')[:-1])
+	length_trimmed = file_name[:16] + '.' + file_ext
+
+	return length_trimmed
+
+def add_number(filename):
+	file_ext = filename.split('.')[-1]
+	file_name = filename.split('.')[0]
+	if '_' in file_name:
+		number = int(file_name.split('_')[-1]) + 1
+		file_name = '_'.join(file_name.split('_')[:-1])
+	else:
+		number = 1
+	return f'{file_name}_{number}.{file_ext}'
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -20,10 +42,16 @@ def upload():
 		if file.filename == '':
 			continue
 		
-		cleaned_filename = file.filename.replace(' ', '_')
+		filename = clean_filename(file.filename)
 		directory = f'uploads/{username}'
 		os.makedirs(directory, exist_ok=True)
-		location = f'{directory}/{cleaned_filename}'
+		location = f'{directory}/{filename}'
+
+		while os.path.exists(location):
+			filename = add_number(filename)
+			print(filename)
+			location = f'{directory}/{filename}'
+			
 		file.save(location)
 	
 	return redirect('/dashboard')
